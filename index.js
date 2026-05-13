62352ac7e6a703a9d4e9d7779e26a27f5274440d
maima5 <null.hma@gmail.com>
2026-05-13 18:59:53 +0700

    benerin port

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal server error' })
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`)
+app.listen(5000, () => {
+  console.log('Backend running on http://localhost:5000')
 })
